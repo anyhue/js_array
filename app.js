7b4a65aabb2955b70d94b78d1d4eb7878c9477d4
anyhue <111332914+anyhue@users.noreply.github.com>
2022-08-21 14:58:31 +0900

    js array

---
+++ app.js
@@ -0,0 +1,23 @@
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+// Get Item from Array
+const daysOfWeek = [mon, tue, wed, thu, fri, sat];
+console.log(daysOfWeek);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+// Practice
+const toDo = ["a", "b", "c"];
+console.log(toDo);
+
+toDo.push("d");
+console.log(toDo);
